e10f548f18c600743fdc42a704cba0aa425340b5
Koen Bossers <bossers.ko@hsleiden.nl>
2020-10-13 16:29:21 +0200

    OKe

--- Testing_kruid_MetaboAnalystR.R
+++ Testing_kruid_MetaboAnalystR.R
@@ -1,4 +1,6 @@
 library(MetaboAnalystR)
+library(xcms)
+
 
 fls <- dir(path = "mzxml", full.names = TRUE)
 pd <- data.frame(file = basename(fls),
@@ -9,9 +11,30 @@ peperdata <- readMSData(fls, pdata = new("NAnnotatedDataFrame", pd),
                    mode = "onDisk") 
 
 
-peperdata2 <- ImportRawMSData("/home/admin_bi/Documents/Micah/LCAB/mzxml/")
 
-param_initial <- SetPeakParam(platform = "general")
-param_optimized <- PerformParamsOptimization(okee, param = param_initial)
+
+peperdata2 <- ImportRawMSData("mzxml/")
+
+
 
 oke <- PerformPeakProfiling(rawData = peperdata, Params = param_initial, plotSettings = SetPlotParam(Plot = TRUE))
+
+kruiden <- PerformDataTrimming("mzxml/", rt.idx = 1)
+param_initial <- SetPeakParam(platform = "general")
+param_optimized <- PerformParamsOptimization(raw_data = kruiden, param = param_initial)
+mSet <- PerformPeakProfiling(rawData = kruiden, Params = param_optimized)
+annParams <- SetAnnotationParam(polarity = "positive")
+annotPeaks <- PerformPeakAnnotation(mSet = mSet, annotaParam = annParams)
+maPeaks <- FormatPeakList(annotPeaks = annotPeaks, annParams = annParams, filtIso = F, filtAdducts = F, missPercent = 1)
+
+mSet<-InitDataObjects("pktable", "stat", FALSE)
+mSet<-Read.TextData(mSet, "metaboanalyst_input.csv", "colu", "disc")
+
+
+
+
+raw_kruid_6 <- PerformDataTrimming("mzxml/", rt.idx = 1)
+param_initial <- SetPeakParam(platform = "general")
+param_optimized <- PerformParamsOptimization(raw_kruid_6, param = param_initial)
+
+mSet <- PerformPeakProfiling(raw_kruid_6, Params = param_initial)
--- Testing_kruid_XCMS.R
+++ Testing_kruid_XCMS.R
@@ -7,7 +7,7 @@ library(magrittr)
 library(png)
 
 # Load data
-kruid_data <- readMSData(files = "Kruid 131 Zwarte peper 6 191119me_71.mzXML", mode = "onDisk")
+kruid_data <- readMSData(files = "mzxml/Kruid 131 Zwarte peper 6 191119me_71.mzXML", mode = "onDisk")
 
 head(fData(kruid_data))
 rtime(kruid_data)
@@ -15,4 +15,15 @@ rtime(kruid_data)
 kruid_chr <- chromatogram(kruid_data)
 plot(kruid_chr)
 
-plot(chromatogram(data_cent))
+# default centwave parameters:
+cwp <- CentWaveParam()
+
+# dry-run peak detection on XIC:
+res <- findChromPeaks(kruid_chr, param = cwp)
+chromPeaks(res)
+plot(res)
+
+cwp <- CentWaveParam(peakwidth = c(2, 10), integrate = 2)
+srn_chr <- findChromPeaks(srn_chr, param = cwp)
+plot(srn_chr)
+chromPeaks(srn_chr)
