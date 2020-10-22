8acd5753eefa9c779cbea52b6155ac3a5ca7841b
Micah <mkmallee@hotmail.com>
2020-10-22 18:07:42 +0200

    Data loading 1

---
+++ shiny/data_loading.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(MetaboAnalystR)
+
+# Define UI
+ui <- fluidPage(
+  fileInput(inputId = "raw_data", label = "Choose mzXML file", multiple = TRUE, accept = '.mzxml',),
+  mainPanel(
+    textOutput('path')
+  )
+  
+)
+
+server <- function(input, output){
+  output$path <- renderText(expr = input$raw_data
+  )
+}
+
+shinyApp(ui, server)
