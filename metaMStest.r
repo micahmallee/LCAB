a1108e13ed5eac885430cc394daff94a460470e3
Koen Bossers <bossers.ko@hsleiden.nl>
2021-02-23 16:54:10 +0100

    asdasd

--- metaMStest.r
+++ metaMStest.r
@@ -7,7 +7,7 @@ data(FEMsettings)
 # Complete metaMS workflow
 cdfdir <- system.file('extdata', package = 'metaMSdata')
 cdffiles <- list.files(cdfdir, pattern = "_GC_", full.names = T, ignore.case = T)
-result <- runGC(files = cdffiles, settings = TSQXLS.GC, DB = DB, nSlaves = 2)
+result1 <- runGC(files = cdffiles, settings = TSQXLS.GC, DB = mmsdb, nSlaves = 2)
 
 # Whole proces behind runGC:
 ## Peak picking
@@ -19,6 +19,7 @@ sapply(allSamples.msp, length)
 plotPseudoSpectrum(allSamples.msp[[1]][[26]])
 ## Annotation
 DB.treated <- treat.DB(DB)
+DB.treated <- treat.DB(mmsdb)
 allSam.matches <- matchSamples2DB(allSamples.msp, DB = DB.treated, settings = metaSetting(TSQXLS.GC, "match2DB"), quick = FALSE)
 allSam.matches
 matchExpSpec(allSamples.msp[[1]][[4]], DB.treated, DB.treated = TRUE, plotIt = TRUE)
@@ -38,122 +39,126 @@ result <- list(PeakTable = cbind(data.frame(features.df), data.frame(round(ann.d
      xset = allSamples, annotation = allSam.matches$annotation, samples.msp = allSamples.msp, SessionInfo = sessionInfo())
 
 
+
+sampletje <- runCAMERA(xset = GCset[[1]], chrom = 'GC', settings = metaSetting(TSQXLS.GC, "CAMERA"))
+mspsampletje <- to.msp(object = sampletje, settings = metaSetting(TSQXLS.GC, 'DBconstruction'))
+DB.treated <- treat.DB(DB)
+sampletjematches <- matchSamples2DB(list(mspsampletje), DB = DB.treated, settings = metaSetting(TSQXLS.GC, "match2DB"), quick = FALSE)
+
+
+
+
 # Own data:
 mmsdb <- read.msp(file = 'MoNA-export-GC-MS_Spectra.msp')
 mmssettings <- TSQXLS.GC
-## Make data suitable for runCAMERA? or for to.msp
+## Make data suitable for runCAMERA? or for to.msp # xsannotat + groupFWHM = runCAMERA
 xsetxcms <- xsAnnotate(xs = smSet$xcmsSet)
 ### Create pseudospectra
 groupedxset <- groupFWHM(object = xsetxcms)
-
-# xsetCam <- runCAMERA(smSet$xcmsSet, chrom = "GC", settings = metaSetting(TSQXLS.GC, "CAMERA"))
-
-okemsp <- to.msp(groupedxset, file = NULL, settings = metaSetting(TSQXLS.GC, "DBconstruction"))
-
-okemsp <- result
-okematches <- matchSamples2DB(okemsp, DB = DB.treated, settings = metaSetting(TSQXLS.GC, "match2DB"), quick = FALSE)
-
-
-
-## Definition of pseudospectra
-allSamples <- lapply(GCset, runCAMERA, chrom = "GC", settings = metaSetting(TSQXLS.GC, "CAMERA"))
-allSamples.msp <- lapply(allSamples, to.msp, file = NULL, settings = metaSetting(TSQXLS.GC, "DBconstruction"))
-sapply(allSamples.msp, length)
-plotPseudoSpectrum(allSamples.msp[[1]][[26]])
-## Annotation
+### Correct format peakinfo
+convertedgroupedxset <- groupedxset
+convertedgroupedxset@groupInfo <- convertedgroupedxset@xcmsSet@peaks
+### Convert pseudospectra to MSP format
+xsetmsp <- to.msp(object = convertedgroupedxset, settings = metaSetting(TSQXLS.GC, 'DBconstruction'))
+length(xsetmsp)
+### Annotation
 DB.treated <- treat.DB(DB)
-allSam.matches <- matchSamples2DB(allSamples.msp, DB = DB.treated, settings = metaSetting(TSQXLS.GC, "match2DB"), quick = FALSE)
-allSam.matches
-matchExpSpec(allSamples.msp[[1]][[4]], DB.treated, DB.treated = TRUE, plotIt = TRUE)
+nestedxsetmsp <- list(xsetmsp)
+xsetallmatches <- matchSamples2DB(xset.msp = nestedxsetmsp, DB = DB.treated, settings = metaSetting(TSQXLS.GC, "match2DB"), quick = FALSE)
+xsetallmatches <- matchSamples2DB(xset.msp = nestedxsetmsp, DB = DB.treated, settings = settings, quick = FALSE)
+xsetallmatches
+matchExpSpec(xsetallmatches[[1]][[1]], DB.treated, DB.treated = TRUE, plotIt = TRUE)
 ## Unknowns
-allSamples.msp.scaled <- lapply(allSamples.msp, treat.DB, isMSP = F)
-allSamples.matches <- matchSamples2Samples(allSamples.msp.scaled, allSamples.msp, annotations = allSam.matches$annotations, settings = metaSetting(TSQXLS.GC, "betweenSamples"))
-names(allSamples.matches)
-## Output
-features.df <- getFeatureInfo(stdDB = DB, allMatches = allSamples.matches, sampleList = allSamples.msp)
-features.df[, c(1:3, ncol(features.df) - 2:0)]
-PseudoSpectra <- constructExpPseudoSpectra(allMatches = allSamples.matches, standardsDB = DB)
-ann.df <- getAnnotationMat(exp.msp = allSamples.msp, pspectra = PseudoSpectra, allMatches = allSamples.matches)
-ann.df
-ann.df2 <- sweep(ann.df, 1, sapply(PseudoSpectra, function(x) max(x$pspectrum[, 2])), FUN = "*")
-ann.df2
-result <- list(PeakTable = cbind(data.frame(features.df), data.frame(round(ann.df2))), PseudoSpectra = PseudoSpectra, settings = TSQXLS.GC, 
-               xset = allSamples, annotation = allSam.matches$annotation, samples.msp = allSamples.msp, SessionInfo = sessionInfo())
+nestedxsetmsp.scaled <- lapply(nestedxsetmsp, treat.DB, isMSP = F)
+nestedxsetmsp.scaled <- list(nestedxsetmsp.scaled)
 
+####################################################################
+xsetallmatches_unknowns <- matchSamples2Samples(nestedxsetmsp.scaled, nestedxsetmsp, annotations = xsetallmatches$annotations, settings = metaSetting(TSQXLS.GC, "betweenSamples"))
+names(xsetallmatches_unknowns)
 
 
 # Convert MAR xcmsSet to xcms xcmsSet, no method for conversion, since slots are the same, loop through.
-xset <- smSet$xcmsSet
-annxset <- xsAnnotate(xs = xset, sample = c(1:2), polarity = 'positive')
-setjesgroeperen <- groupFWHM(object = annxset)
-
 for (i in slotNames(xset)) {
   slot(lekkersetjehoor, i) <- slot(xset, i)
 }
 
-lijstje <- list()
-lijstje[[1]] <- xs
 
 
-object = xsetCam
-xsetCam@groupInfo <- smSet$xcmsSet@peaks
 
 
-function (object, file = NULL, settings = NULL, ndigit = 0, minfeat, 
-          minintens, intensity = c("maxo", "into"), secs2mins = TRUE) 
-{
-  if (!is.null(settings)) {
-    intensity <- settings$intensityMeasure
-    minfeat <- settings$minfeat
-    minintens <- settings$minintens
+
+
+
+
+xset.msp <- allSamples.msp
+DB = DB.treated2
+settings = metaSetting(TSQXLS.GC, 'match2DB')
+quick = F
+
+
+matchSamples2DB <- function(xset.msp, DB, settings, quick) {
+  if (settings$timeComparison == "RI") {
+    standard.rts <- sapply(DB, function(x) x$std.RI)
+    ## rt.matches is a list of two-column matrices, one for each xset.msp element. The first column gives the DB entry that
+    ## matches with the sample entry in the second column. This is very fast to calculate.
+    rt.matches <- lapply(1:length(xset.msp), function(ii) {
+      group.rts <- sapply(xset.msp[[ii]], function(x) mean(x[, "RI"]))
+      which(abs(outer(standard.rts, group.rts, FUN = "-")) < settings$RIdiff, arr.ind = TRUE)
+    })
+  } else {
+    standard.rts <- sapply(DB, function(x) x$std.rt)
+    
+    rt.matches <- lapply(1:length(xset.msp), function(ii) {
+      group.rts <- sapply(xset.msp[[ii]], function(x) mean(x[, "rt"]))
+      which(abs(outer(standard.rts, group.rts, FUN = "-")) < settings$rtdiff, arr.ind = TRUE)
+    })
   }
-  else {
-    intensity <- match.arg(intensity)
-  }
-  if (class(object) == "xsAnnotate") {
-    allpks <- object@groupInfo
-    minI <- minintens * max(allpks[, intensity])
-    tooSmall <- which(allpks[, intensity] < minI)
-    pspectra <- lapply(object@pspectra, function(x) x[!x %in% 
-                                                        tooSmall])
-  }
-  else {
-    minI <- minintens * max(object[, intensity])
-    allpks <- object[object[, intensity] >= minI, ]
-    pspectra <- split(1:nrow(allpks), allpks[, "rt"])
-  }
-  npeaks <- sapply(pspectra, length)
-  pspectra <- pspectra[npeaks >= minfeat]
-  if (!is.null(file)) {
-    if (length(pspectra) > 0) {
-      for (i in 1:length(pspectra)) {
-        ofile <- paste(file, "_", ceiling(i/1000), ".txt", 
-                       sep = "")
-        newfile <- (i%%1000) == 1
-        idx <- pspectra[[i]]
-        pks <- allpks[idx, , drop = FALSE]
-        pks <- pks[order(pks[, "mz"]), , drop = FALSE]
-        pks[, intensity] <- 1000 * pks[, intensity]/max(pks[, 
-                                                            intensity])
-        cat("Name: grp ", i, " (rt: ", mean(pks[, "rt"]), 
-            ")", sep = "", file = ofile, append = !newfile)
-        cat("\nNum Peaks:", nrow(pks), file = ofile, 
-            append = TRUE)
-        for (ii in 1:nrow(pks)) cat("\n(", round(pks[ii, 
-                                                     "mz"], ndigit), "\t", round(pks[ii, intensity], 
-                                                                                 ndigit), ")", file = ofile, append = TRUE, 
-                                    sep = "")
-        cat("\n\n", file = ofile, append = TRUE)
+  
+  if (quick) {
+    ## xset.msp has already been scaled
+    match.results <- lapply(1:length(xset.msp), function(ii) {
+      if (nrow(rt.matches[[ii]] > 0)) {
+        result <- matrix(0, length(DB), length(xset.msp[[ii]]))
+        for (i in 1:nrow(rt.matches[[ii]])) {
+          DB.idx <- rt.matches[[ii]][i, 1]
+          sample.idx <- rt.matches[[ii]][i, 2]
+          result[DB.idx, sample.idx] <- mzmatch(DB[[DB.idx]]$pspectrum, xset.msp[[ii]][[sample.idx]])
+        }
       }
-    }
+      result
+    })
+  } else {
+    ## scaling is done for each comparison separately, since high mz values may be removed depending on MonoMW of the standard
+    ## compound. Slower, obviously.
+    match.results <- lapply(1:length(xset.msp), function(ii) {
+      result <- matrix(0, length(DB), length(xset.msp[[ii]]))
+      if (nrow(rt.matches[[ii]] > 0)) {
+        for (i in 1:nrow(rt.matches[[ii]])) {
+          DB.idx <- rt.matches[[ii]][i, 1]
+          sample.idx <- rt.matches[[ii]][i, 2]
+          exp.pat <- xset.msp[[ii]][[sample.idx]]
+          
+          MWlimit <- DB[[DB.idx]]$monoMW + 4
+          ## sometimes, with manually added spectra, monoMW is not present... then we use everything up until the highest mass present.
+          if (length(MWlimit) == 0) 
+            MWlimit <- max(DB[[DB.idx]]$pspectrum[, 1])
+          
+          ok.mz <- which(exp.pat[, "mz"] <= MWlimit)
+          if (length(ok.mz) > settings$minfeat) {
+            exp.pat <- treat.DB(list(exp.pat[ok.mz, ]), isMSP = FALSE)
+            result[DB.idx, sample.idx] <- metaMS:::mzmatch(DB[[DB.idx]]$pspectrum, exp.pat[[1]])
+          }
+        }
+      }
+      result
+    })
   }
-  result <- metaMS:::removeDoubleMasses(lapply(pspectra, function(x) cbind(mz = round(allpks[x, 
-                                                                                    "mz"], digits = ndigit), allpks[x, c(intensity, "rt")])))
-  if (secs2mins) {
-    invisible(lapply(result, function(x) cbind(x[, c("mz", 
-                                                     intensity)], rt = x[, "rt"]/60)))
-  }
-  else {
-    invisible(result)
-  }
+  names(match.results) <- names(xset.msp)
+  
+  annotations <- lapply(match.results, function(xx) {
+    sapply(1:ncol(xx), function(ii) which(xx[, ii] > settings$simthresh))
+  })
+  
+  list(annotations = mapply(annotations2tab, annotations, match.results, SIMPLIFY = FALSE))
 }
+
