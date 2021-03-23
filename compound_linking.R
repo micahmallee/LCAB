2e6344e8f3ab340d90891ebabf3d008e36495fe5
Koen Bossers <bossers.ko@hsleiden.nl>
2021-03-23 16:52:09 +0100

    asd

--- compound_linking.R
+++ compound_linking.R
@@ -16,22 +16,33 @@ test_data <- readMSData('test_data_mzxml/Test_nieuwe_kolom_MCX_SPE_KW1_Spike_18.
 trimmed_test_data <- PerformDataTrimming(datapath = c("test_data_mzxml/"), rt.idx = 1, plot = F)
 params_opt <- PerformParamsOptimization(raw_data = trimmed_test_data, param = SetPeakParam(platform = 'general', Peak_method = 'centWave'))
 
+params_pre <- SetPeakParam(platform = 'general', Peak_method = 'centWave')
+
+
 p23 <- Noise_evaluate(test_data)
-params <- SetPeakParam(ppm = 50, noise = p23$noise, value_of_prefilter = p23$value_of_prefilter, prefilter = p23$prefilter)
+params <- SetPeakParam(ppm = p23$ppm, noise = p23$noise, value_of_prefilter = p23$value_of_prefilter, prefilter = p23$prefilter, min_peakwidth = 2, max_peakwidth = 10)
 smSet_test_data <- PerformPeakPicking(test_data, param = updateRawSpectraParam(params))
 smSet_test_data[["onDiskData"]]@phenoData@data[["sample_name"]] <- smSet_test_data[["onDiskData"]]@phenoData@data[["sampleNames"]]
 smSet_test_data[["onDiskData"]]@phenoData@data[["sampleNames"]] <- NULL
-# smSet_test_data <- PerformPeakAlignment(smSet_test_data, param = updateRawSpectraParam(params))
-# smSet_test_data <- PerformPeakFiling(smSet_test_data, param = updateRawSpectraParam(params))
-oke <- PerformPeakAnnotation(mSet = smSet_test_data, annotaParam = SetAnnotationParam())
 
-oke1 <- new(Class = 'xcmsSet')
+test_data_xcms <- mSet2xcmsSet(smSet_test_data)
+test_data_xsannotate <- xsAnnotate(test_data_xcms)
+test_data_xsannotate <- groupFWHM(test_data_xsannotate)
+test_data_msp <- to.msp(object = test_data_xsannotate, file = NULL, settings = NULL, ndigit = 3, minfeat = 1, minintens = 0, intensity = "maxo", secs2mins = F)
+test_data_msp <- to.msp(object = ja2,settings = metaSetting(TSQXLS.GC, 'DBconstruction'), file = NULL, intensity = "maxo")
 
-ja <- xcms::findChromPeaks(object = test_data, param = CentWaveParam(ppm = 22, peakwidth = c(2, 10), prefilter = c(0, 0) ))
-ja1 <- xcms::findChromPeaks(object = test_data, param = CentWaveParam(ppm = 22, peakwidth = c(2, 10), prefilter = c(p23$prefilter, p23$value_of_prefilter) ))
-ja2 <- xsAnnotate(ja1)
+querysplash <- getsplashscores(list(test_data_msp))
+query_secondblocks <- getsecondblocks(querysplash)
+splashmatches <- matchsecondblocks(querysecondblocks = query_secondblocks, databasesecondblocks = mona_secondblocks)
+similarity_scores <- similarities(msp_query = list(test_data_msp), database = mona_msp, SPLASH_hits = splashmatches)
+bestmatches <- tophits(similarity_scores = similarity_scores, limit = 5, database = mona_msp, splashmatches = splashmatches)
+
+
+ja <- xcms::findChromPeaks(object = test_data, param = CentWaveParam(ppm = 5, peakwidth = c(1, 30), prefilter = c(0, 0) ))
+ja1 <- xcms::findChromPeaks(object = test_data, param = CentWaveParam(ppm = 5, peakwidth = c(2, 10), prefilter = c(p23$prefilter, p23$value_of_prefilter)))
+ja2 <- xsAnnotate(okee)
 ja2 <- groupFWHM(ja2)
-jamsp <- to.msp(object = ja2,settings = metaSetting(TSQXLS.GC, 'DBconstruction'), file = 'okee.msp')
+jamsp <- to.msp(object = ja2,settings = metaSetting(TSQXLS.GC, 'DBconstruction'))
 
 querysplash <- getsplashscores(list(jamsp))
 query_secondblocks <- getsecondblocks(querysplash)
@@ -41,36 +52,181 @@ bestmatches <- tophits(similarity_scores = similarity_scores, limit = 5, databas
 
 for (i in 1:length(bestmatches)) {
   try(expr = {
-    print(paste0('Best match: ', bestmatches[[i]][[1]][[1]][["Name"]], ' Score: ', bestmatches[[i]][[1]][["Score"]]))
+    print(paste0(i, '  Best match: ', bestmatches[[i]][[1]][[1]][["Name"]], ' Score: ', bestmatches[[i]][[1]][["Score"]]))
   }, silent = T)
 }
 
+
 #' Spiked data:
-#' L-Valine, TMS derivative
-#' Glycine, 3TMS derivative
-#' Serine, 3TMS derivative
-#' L-Threonine, 3TMS derivative
-#' L-Hydroxyproline, (E)-, 3TMS derivative
-#' DL-Phenylalanine, TMS derivative
-#' Phenylalanine, 2TMS derivative
-#' Octopamine, 3TMS derivative
+#' L-Valine, TMS derivative       Nee
+#' Glycine, 3TMS derivative       half, wel .86 similarity, via MONA website wel!
+#' Serine, 3TMS derivative        Nee
+#' L-Threonine, 3TMS derivative   Nee
+#' L-Hydroxyproline, (E)-, 3TMS derivative    Ja (36)
+#' DL-Phenylalanine, TMS derivative         ja 
+#' Phenylalanine, 2TMS derivative   
+#' Octopamine, 3TMS derivative      Denk het wel, zit niet in MoNA
 #' L-Lysine, 4TMS derivative        OUI MONSIEUR
-#' Octopamine, 4TMS derivative      
-#' Palmitic Acid, TMS derivative    
-#' Serotonin, 4TMS derivative       
+#' Octopamine, 4TMS derivative      Denk het wel, zit niet in MoNA
+#' Palmitic Acid, TMS derivative    Ja zit erin (andere splash code?)
+#' Serotonin, 4TMS derivative       ja 5-Hydoxytryptamine
 
 oke <- vector(length(test1[[1]]), mode = 'numeric')
 oke2 <- vector(length(test1[[1]]), mode = 'numeric')
 for (i in 1:length(test1[[1]])) {
   woord <- str_split(test1[[1]][i], pattern = ',')
   woord[[1]][2]
-  oke[[i]] <- woord[[1]][1]
-  oke2[[i]] <- woord[[1]][2]
+  oke[[i]] <- as.numeric(woord[[1]][1])
+  oke2[[i]] <- as.numeric(woord[[1]][2])
 }
 
-lapply(test1[[1]], function(x) {
-  print(list(str_split(x, ','))[[1]][[1]][[2]])
-})
+oke3 <- rbind(oke, oke2)
+oke3 <- t(oke3)
+oke3 <- data.frame(oke3)
+
+L_valine_TMS <- oke3
+Glycine_TMS <- oke3
+Serine_TMS <- oke3
+L_Threonine_TMS <- oke3
+
+DL_Phenylalanine_TMS <- oke3
+Phenylalanine_TMS <- oke3
+Palmatic_acid_TMS <- oke3
+
+L_valine_results <- compare_single_compound(L_valine_TMS, test_data_msp)
+Glycine_results <- compare_single_compound(Glycine_TMS, test_data_msp)
+Serine_results <- compare_single_compound(Serine_TMS, test_data_msp)
+L_Threonine_results <- compare_single_compound(compound = L_Threonine_TMS, test_data_msp)
+
+DL_Phenylalanine_results <- compare_single_compound(DL_Phenylalanine_TMS, test_data_msp)
+Phenylalanine_results <- compare_single_compound(compound = Phenylalanine_TMS, database = test_data_msp)
+Palmatic_acid_results <- compare_single_compound(Palmatic_acid_TMS, database = test_data_msp)
+
+silat_results <- compare_single_compound(compound = silat[[1]]$pspectrum, database = jamsp)
+
+
+
+compare_single_compound <- function(compound, database) {
+  # compoundspectrum <- new('Spectrum2', mz = compound[, 1], intensity = compound[, 2])
+  results <- vector(mode = 'list', length = length(database))
+  results <- lapply(database, function(x) {
+    SpectrumSimilarity(spec.top =  x[, 1:2], spec.bottom = compound, print.alignment = F, print.graphic = F)
+    # checkspectrum <- new('Spectrum2', mz = x[, 1], intensity = x[, 2])
+    # compareSpectra(checkspectrum, compoundspectrum)
+  })
+  return(results)
+}
+
+
+oke <- c(rbind(jamsp[[1]][, 1], jamsp[[1]][, 2]))
+
+
+oke <- c(rbind(test_data_msp[[20]][, 1], test_data_msp[[20]][, 2]))
+
+
+
+
+function (spec.top, spec.bottom, t = 0.25, b = 10, top.label = NULL, 
+          bottom.label = NULL, xlim = c(50, 1200), x.threshold = 0, 
+          print.alignment = FALSE, print.graphic = TRUE, output.list = FALSE) 
+{
+  top_tmp <- data.frame(mz = spec.top[, 1], intensity = spec.top[, 
+                                                                 2])
+  top_tmp$normalized <- round((top_tmp$intensity/max(top_tmp$intensity)) * 
+                                100)
+  top_tmp <- subset(top_tmp, top_tmp$mz >= xlim[1] & top_tmp$mz <= 
+                      xlim[2])
+  top_plot <- data.frame(mz = top_tmp$mz, intensity = top_tmp$normalized)
+  top <- subset(top_plot, top_plot$intensity >= b)
+  bottom_tmp <- data.frame(mz = spec.bottom[, 1], intensity = spec.bottom[, 
+                                                                          2])
+  bottom_tmp$normalized <- round((bottom_tmp$intensity/max(bottom_tmp$intensity)) * 
+                                   100)
+  bottom_tmp <- subset(bottom_tmp, bottom_tmp$mz >= xlim[1] & 
+                         bottom_tmp$mz <= xlim[2])
+  bottom_plot <- data.frame(mz = bottom_tmp$mz, intensity = bottom_tmp$normalized)
+  bottom <- subset(bottom_plot, bottom_plot$intensity >= b)
+  for (i in 1:nrow(bottom)) top[, 1][bottom[, 1][i] >= top[, 
+                                                           1] - t & bottom[, 1][i] <= top[, 1] + t] <- bottom[, 
+                                                                                                              1][i]
+  alignment <- merge(top, bottom, by = 1, all = TRUE)
+  if (length(unique(alignment[, 1])) != length(alignment[, 
+                                                         1])) 
+    warning("the m/z tolerance is set too high")
+  alignment[, c(2, 3)][is.na(alignment[, c(2, 3)])] <- 0
+  names(alignment) <- c("mz", "intensity.top", "intensity.bottom")
+  if (print.alignment == TRUE) {
+    print(alignment)
+  }
+  if (x.threshold < 0) 
+    stop("x.threshold argument must be zero or a positive number")
+  alignment <- alignment[alignment[, 1] >= x.threshold, ]
+  u <- alignment[, 2]
+  v <- alignment[, 3]
+  similarity_score <- as.vector((u %*% v)/(sqrt(sum(u^2)) * sqrt(sum(v^2))))
+  if (print.graphic == TRUE) {
+    plot.new()
+    plot.window(xlim = xlim, ylim = c(-125, 125))
+    ticks <- c(-100, -50, 0, 50, 100)
+    for (i in 1:length(top_plot$mz)) lines(rep(top_plot$mz[i], 
+                                               2), c(0, top_plot$intensity[i]), col = "blue")
+    for (i in 1:length(bottom_plot$mz)) lines(rep(bottom_plot$mz[i], 
+                                                  2), c(0, -bottom_plot$intensity[i]), col = "red")
+    axis(2, at = ticks, labels = abs(ticks), pos = xlim[1], 
+         ylab = "intensity")
+    axis(1, pos = -125)
+    lines(xlim, c(0, 0))
+    rect(xlim[1], -125, xlim[2], 125)
+    mtext("m/z", side = 1, line = 2)
+    mtext("intensity (%)", side = 2, line = 2)
+    plot.window(xlim = c(0, 20), ylim = c(-10, 10))
+    text(10, 9, top.label)
+    text(10, -9, bottom.label)
+  }
+  if (output.list == TRUE) {
+    headTailPlot <- function() {
+      pushViewport(plotViewport(c(5, 5, 2, 2)))
+      pushViewport(dataViewport(xscale = xlim, yscale = c(-125, 
+                                                          125)))
+      grid.rect()
+      tmp <- pretty(xlim)
+      xlabels <- tmp[tmp >= xlim[1] & tmp <= xlim[2]]
+      grid.xaxis(at = xlabels)
+      grid.yaxis(at = c(-100, -50, 0, 50, 100))
+      grid.segments(top_plot$mz, top_plot$intensity, top_plot$mz, 
+                    rep(0, length(top_plot$intensity)), default.units = "native", 
+                    gp = gpar(lwd = 0.75, col = "blue"))
+      grid.segments(bottom_plot$mz, -bottom_plot$intensity, 
+                    bottom_plot$mz, rep(0, length(bottom_plot$intensity)), 
+                    default.units = "native", gp = gpar(lwd = 0.75, 
+                                                        col = "red"))
+      grid.abline(intercept = 0, slope = 0)
+      grid.text("intensity (%)", x = unit(-3.5, "lines"), 
+                rot = 90)
+      grid.text("m/z", y = unit(-3.5, "lines"))
+      popViewport(1)
+      pushViewport(dataViewport(xscale = c(0, 20), yscale = c(-10, 
+                                                              10)))
+      grid.text(top.label, unit(10, "native"), unit(9, 
+                                                    "native"))
+      grid.text(bottom.label, unit(10, "native"), unit(-9, 
+                                                       "native"))
+      popViewport(2)
+    }
+    p <- grid.grabExpr(headTailPlot())
+  }
+  if (output.list == FALSE) {
+    return(similarity_score)
+  }
+  if (output.list == TRUE) {
+    return(list(similarity.score = similarity_score, alignment = alignment, 
+                plot = p))
+  }
+}
+
+
+
+
 
 # Compound linking
 ## Data loading, database, peakprofiling etc
@@ -234,4 +390,31 @@ split_annotate <- function(mSet) {
     x <- xsAnnotate(x)
     x <- groupFWHM(x)
   })
+}
+
+mSet2xcmsSet <-  function(mSet = smSet_test_data) {
+  xs <- new("xcmsSet")
+  xs@peaks <- mSet[["msFeatureData"]][["chromPeaks"]]
+  rts <- list()
+  if (class(mSet[[2]]) == "OnDiskMSnExp") {
+    format <- "onDiskData"
+  }
+  else {
+    format <- "inMemoryData"
+  }
+  rts$raw <- rtime(mSet[[format]])
+  rts$corrected <- mSet[["msFeatureData"]][["adjustedRT"]]
+  xs@rt <- rts
+  xs@phenoData <- pData(mSet[[format]])
+  xs@filepaths <- fileNames(mSet[[format]])
+  profMethod <- NA
+  profStep <- NA
+  profParam <- list()
+  xs@mslevel <- 1
+  xs@scanrange <- range(scanIndex(mSet[[format]]))
+  if (any(mSet$msFeatureData$chromPeakData$is_filled)) {
+    fld <- which(mSet$msFeatureData$chromPeakData$is_filled)
+    xs@filled <- as.integer(fld)
+  }
+  return(xs)
 }
