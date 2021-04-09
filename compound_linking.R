d8b33c1295a4e8e073f99bf20055cd3784c6ca19
Micah <mkmallee@hotmail.com>
2021-04-09 21:38:49 +0200

    asdf

--- compound_linking.R
+++ compound_linking.R
@@ -24,7 +24,7 @@ test_data <- readMSData('test_data_mzxml/raw/spike18.mzXML', mode = 'onDisk')
 # params_opt <- PerformParamsOptimization(raw_data = trimmed_test_data, param = SetPeakParam(platform = 'general', Peak_method = 'centWave'))
 ### Peak detection
 p23 <- Noise_evaluate(test_data)
-params <- SetPeakParam(ppm = 22, noise = p23$noise, value_of_prefilter = p23$value_of_prefilter, prefilter = p23$prefilter, min_peakwidth = 2, max_peakwidth = 8, snthresh = 100, mzdiff = 0.1)
+params <- SetPeakParam(ppm = 22, noise = p23$noise, value_of_prefilter = p23$value_of_prefilter, prefilter = p23$prefilter, min_peakwidth = 1, max_peakwidth = 15, snthresh = 10, mzdiff = 0.1)
 smSet_test_data <- PerformPeakPicking(test_data, param = updateRawSpectraParam(params))
 smSet_test_data[["onDiskData"]]@phenoData@data[["sample_name"]] <- smSet_test_data[["onDiskData"]]@phenoData@data[["sampleNames"]]
 smSet_test_data[["onDiskData"]]@phenoData@data[["sampleNames"]] <- NULL
@@ -32,19 +32,19 @@ smSet_test_data[["onDiskData"]]@phenoData@data[["sampleNames"]] <- NULL
 ### Get pseudospectra and convert to msp format
 test_data_xcms <- mSet2xcmsSet(smSet_test_data)
 test_data_xsannotate <- xsAnnotate(test_data_xcms)
-test_data_xsannotate <- groupFWHM(test_data_xsannotate, perfwhm = 3)
+test_data_xsannotate <- groupFWHM(test_data_xsannotate, perfwhm = 1)
 test_data_msp <- to.msp(object = test_data_xsannotate, file = NULL, settings = NULL, ndigit = 3, minfeat = 5, minintens = 0, intensity = "maxo", secs2mins = F)
 
 ### Get SPLASH hashes and match thirdblocks
-querySPLASH <- get_splashscores(msp_object = list(test_data_msp))
+querySPLASH <- get_splashscores(msp_list = list(test_data_msp))
 full_mona_SPLASHES <- vector(mode = 'character', length = length(mona_msp))
 full_mona_SPLASHES <- sapply(mona_msp, function(x){
   str_extract(string = x$Comments, pattern = regex('SPLASH=splash10................'))
 })
-query_thirdblocks <- get_blocks(splashscores = querySPLASH, blocknr = 3)
+query_thirdblocks <- lapply(querySPLASH, get_blocks, blocknr = 3)
 database_thirdblocks <- get_blocks(splashscores = full_mona_SPLASHES, blocknr = 3)
-SPLASH_matches <- match_nines(query_blocks = query_thirdblocks, database_blocks = database_thirdblocks)
-similarity_scores <- similarities_thirdblocks(msp_query = test_data_msp, database = mona_msp, nine_matches = SPLASH_matches)
+SPLASH_matches <- lapply(query_thirdblocks, match_nines, database_blocks = database_thirdblocks)
+similarity_scores <- similarities_thirdblocks(nine_matches = SPLASH_matches, msp_query = test_data_msp, database = mona_msp)
 
 ### Get x top matches
 bestmatches <- tophits(similarity_scores = similarity_scores, limit = 15, database = mona_msp, splashmatches = SPLASH_matches)
@@ -55,40 +55,24 @@ matchmatrix <- t(data.table::rbindlist(bestmatches))
 ## Multiple samples
 msamples <- readMSData(c('mzxml/KRUID_126/Kruid 126 Zwarte peper 1 191119me_66.mzXML', 'mzxml/KRUID_131/Kruid 131 Zwarte peper 6 191119me_71.mzXML'), mode = 'onDisk')
 p24 <- Noise_evaluate(msamples)
-params2 <- SetPeakParam(ppm = 22, noise = p24$noise, value_of_prefilter = p24$value_of_prefilter, prefilter = p24$prefilter, min_peakwidth = 2, max_peakwidth = 8, snthresh = 100, mzdiff = 0.1)
+params2 <- SetPeakParam(ppm = p24$ppm, noise = p24$noise, value_of_prefilter = p24$value_of_prefilter, prefilter = p24$prefilter, min_peakwidth = 2, max_peakwidth = 8, snthresh = 100, mzdiff = 0.1)
 smSet_msamples <- PerformPeakPicking(msamples, param = updateRawSpectraParam(params2))
 smSet_msamples[["onDiskData"]]@phenoData@data[["sample_name"]] <- smSet_msamples[["onDiskData"]]@phenoData@data[["sampleNames"]]
 smSet_msamples[["onDiskData"]]@phenoData@data[["sampleNames"]] <- NULL
 smSet_msamples <- PerformPeakAlignment(smSet_msamples, param = updateRawSpectraParam(params2))
 smSet_msamples <- PerformPeakFiling(smSet_msamples, param = updateRawSpectraParam(params2))
 xcmslist <-  split_annotate(mSet = smSet_msamples)
+msplist <- lapply(xcmslist, to.msp, file = NULL, settings = NULL, ndigit = 3, minfeat = 5, minintens = 0, intensity = "maxo", secs2mins = F)
 
-
-split_annotate <- function(mSet) {
-  #' This function splits the mSet object based on sample. 
-  #' Subsequently this function groups the peaks into pseudospectra.
-  f <- vector(mode = 'character', length = length(mSet$xcmsSet@phenoData$sample_name))
-  f <- sapply(mSet$xcmsSet@phenoData$sample_name, function(x) {
-    gsub(x = x, pattern = " ", replacement =  ".", fixed = T)
-  })
-  splitxcms <- xcms:::split.xcmsSet(mSet$xcmsSet, f = factor(f))
-  annotatedxcmslist <- vector(mode = 'list', length = length(f))
-  annotatedxcmslist <- lapply(splitxcms, function(x){
-    x <- xsAnnotate(x)
-    x <- groupFWHM(x, perfwhm = 3)
-  })
-}
-
-
+## Compare multiple sample approach to single sample approach
 blabla <- readMSData('kruiden/Kruid 126 Zwarte peper 1 191119me_66.mzXML', mode = 'onDisk')
-blabla1 <- PerformPeakPicking(blabla, param = updateRawSpectraParam(params))
+blabla1 <- PerformPeakPicking(blabla, param = updateRawSpectraParam(params2))
 blabla1[["onDiskData"]]@phenoData@data[["sample_name"]] <- blabla1[["onDiskData"]]@phenoData@data[["sampleNames"]]
 blabla1[["onDiskData"]]@phenoData@data[["sampleNames"]] <- NULL
-
-### Get pseudospectra and convert to msp format
 blablaxcms <- mSet2xcmsSet(blabla1)
 blablaxsannotate <- xsAnnotate(blablaxcms)
 blablaxsannotate <- groupFWHM(blablaxsannotate, perfwhm = 3)
+blablamsp <- to.msp(object = blablaxsannotate, file = NULL, settings = NULL, ndigit = 3, minfeat = 5, minintens = 0, intensity = "maxo", secs2mins = F)
 
 
 for (i in 1:length(bestmatches)) {
@@ -194,9 +178,8 @@ tophits <- function(similarity_scores, limit = 5, database, splashmatches) {
     top5_scores <- sort(unlist(similarity_scores[[1]][[x]]), decreasing = T)[1:limit]
     top5_matches <- vector(mode = 'list', length = length(top5_scores))
     top5_matches <- lapply(top5_scores, function(y){
-      # matchandscore <- list(Match = '', Score = 0)
       index1 <- grep(pattern = y, x = unlist(similarity_scores[[1]][[x]]))
-      if (!is.na(splashmatches[[1]][[x]][index1])) {
+      if (!is.na(splashmatches[[1]][[x]][index1]) & as.numeric(y) > 0.8) {
         paste0('Compound: ', as.character(database[[splashmatches[[1]][[x]][index1[1]]]]["Name"]), '   Score: ', round((y * 100), 2))
         # matchandscore[["Match"]] <- as.character(database[[splashmatches[[1]][[x]][index1[1]]]]["Name"])
         # matchandscore[["Score"]] <- round((y * 100), 2)
@@ -209,11 +192,149 @@ tophits <- function(similarity_scores, limit = 5, database, splashmatches) {
   })
 }
 
+similarities_thirdblocks <- function(nine_matches, msp_query, database) {
+  #' Calculate Spectrumsimilarity per SPLASH match
+  # Loop through hits
+  if (length(nine_matches) == 1) {
+    msp_query <- list(msp_query)
+  }
+  if (.Platform$OS.type == 'windows') {
+    num_cores <- detectCores()
+    cl <- makeCluster(num_cores)
+    clusterExport(cl=cl, 'mona_msp')
+    allmatches <- vector(mode = 'list', length = length(nine_matches))
+    allmatches <- lapply(seq_along(nine_matches), function(x) {
+      matches_per_sample <- vector(mode = 'list', length = length(nine_matches[[x]]))
+      matches_per_sample <- parLapply(cl, seq_along(nine_matches[[x]]), function(y){
+        matches_per_pseudospectrum <- vector(mode = 'numeric', length = length(nine_matches[[x]][[y]]))
+        matches_per_pseudospectrum <- sapply(nine_matches[[x]][[y]], function(z){
+          OrgMassSpecR::SpectrumSimilarity(spec.top = msp_query[[x]][[y]][, 1:2], spec.bottom = database[[z]]$pspectrum, print.alignment = F, print.graphic = F)
+        })
+      })
+    })
+    stopCluster(cl)
+  } else {
+    num_cores <- detectCores()
+    allmatches <- vector(mode = 'list', length = length(nine_matches))
+    allmatches <- lapply(seq_along(nine_matches), function(x) {
+      matches_per_sample <- vector(mode = 'list', length = length(nine_matches[[x]]))
+      matches_per_sample <- mclapply(mc.cores = num_cores, seq_along(nine_matches[[x]]), function(y){
+        matches_per_pseudospectrum <- vector(mode = 'numeric', length = length(nine_matches[[x]][[y]]))
+        matches_per_pseudospectrum <- sapply(nine_matches[[x]][[y]], function(z){
+          OrgMassSpecR::SpectrumSimilarity(spec.top = msp_query[[x]][[y]][, 1:2], spec.bottom = database[[z]]$pspectrum, print.alignment = F, print.graphic = F)
+        })
+      })
+    })
+  }
+  return(allmatches)
+}
+
+match_nines <- function(query_blocks, database_blocks) {
+  #' This function takes the vectors containing the third SPLASH blocks for all pseudospectra.
+  #' Matches the position of the character 9 in the the third blocks in database per sample. 
+  #' Returns list with matches per pseudospectra.
+  db_nines <- str_locate(database_blocks, '9')
+  db_nines <- db_nines[, 1]
+  query_nines <- str_locate(query_blocks, '9')
+  query_nines <- query_nines[, 1]
+  indexes_p_sample <- vector(mode = 'list', length = length(query_nines))
+  indexes_p_sample <- lapply(seq_along(query_nines), function(y) {
+    indexes <- sapply(seq_along(db_nines), function(z) ifelse(test = query_nines[[y]] == db_nines[[z]], yes = z, no = 0))
+    indexes <- indexes[indexes != 0]
+  })
+  return(indexes_p_sample)
+}
+
+
+get_blocks <- function(splashscores, blocknr = 3){
+  #' This function takes all n blocks from the SPLASH codes per pseudospectrum.
+  if (class(splashscores) == "character") {
+    blocks <- str_split(string = splashscores, pattern = '-')
+    blocks <- sapply(blocks, function(x) x[[blocknr]])
+  } else {
+    blocks <- vector(mode = "list", length = length(splashscores))
+    blocks <- lapply(seq_along(splashscores), function(x) {
+      blocks_psample <- vector(mode = 'character', length = length(splashscores[[x]]))
+      blocks_psample <- sapply(splashscores[[x]], function(y) {
+        str_split(string = y, pattern = '-', simplify = F)[[1]][[blocknr]]
+      })
+    })
+  }
+  return(blocks)
+}
+
+
+get_splashscores <- function(msp_list) {
+  #' This function calculates all SPLASH codes per pseudospectrum
+  totalsplashscores <- vector(mode = 'list', length = length(msp_list))
+  totalsplashscores <- lapply(msp_list, function(x) {
+    splashscoresquery <- vector(mode = "character", length = length(x))
+    splashscoresquery <- sapply(x, function(y) getSplash(y[, 1:2]))
+  })
+  return(totalsplashscores)
+}
+
+
+mSet2xcmsSet <-  function(mSet) {
+  xs <- new("xcmsSet")
+  xs@peaks <- mSet[["msFeatureData"]][["chromPeaks"]]
+  rts <- list()
+  if (class(mSet[[2]]) == "OnDiskMSnExp") {
+    format <- "onDiskData"
+  }
+  else {
+    format <- "inMemoryData"
+  }
+  rts$raw <- rtime(mSet[[format]])
+  rts$corrected <- mSet[["msFeatureData"]][["adjustedRT"]]
+  xs@rt <- rts
+  xs@phenoData <- pData(mSet[[format]])
+  xs@filepaths <- fileNames(mSet[[format]])
+  xs@mslevel <- 1
+  xs@scanrange <- range(scanIndex(mSet[[format]]))
+  if (any(mSet$msFeatureData$chromPeakData$is_filled)) {
+    fld <- which(mSet$msFeatureData$chromPeakData$is_filled)
+    xs@filled <- as.integer(fld)
+  }
+  return(xs)
+}
+
+split_annotate <- function(mSet) {
+  #' This function splits the mSet object based on sample. 
+  #' Subsequently this function groups the peaks into pseudospectra.
+  f <- vector(mode = 'character', length = length(mSet$xcmsSet@phenoData$sample_name))
+  f <- sapply(mSet$xcmsSet@phenoData$sample_name, function(x) {
+    gsub(x = x, pattern = " ", replacement =  ".", fixed = T)
+  })
+  splitxcms <- xcms:::split.xcmsSet(mSet$xcmsSet, f = factor(f))
+  annotatedxcmslist <- vector(mode = 'list', length = length(f))
+  annotatedxcmslist <- lapply(splitxcms, function(x){
+    x <- xsAnnotate(x)
+    x <- groupFWHM(x, perfwhm = 3)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ################### FIXEN matches per sample wordt niet gebruikt hahahaah
-
-
-
 similarities <- function(msp_query, database, SPLASH_hits) {
   #' Calculate Spectrumsimilarity per SPLASH match
   # Loop through hits
@@ -248,104 +369,4 @@ match_secondblocks <- function(querysecondblocks, databasesecondblocks) {
       grep(pattern = y, x = databasesecondblocks)
     })
   })
-}
-
-similarities_thirdblocks <- function(msp_query, database, nine_matches) {
-  #' Calculate Spectrumsimilarity per SPLASH match
-  # Loop through hits
-  allmatches <- vector(mode = 'list', length = length(nine_matches))
-  allmatches <- lapply(seq_along(nine_matches), function(x){
-    matches_per_sample <- vector(mode = 'list', length = length(nine_matches[[x]]))
-    matches_per_sample <- lapply(seq_along(nine_matches[[x]]), function(y){
-      matches_per_pseudospectrum <- vector(mode = 'numeric', length = length(nine_matches[[x]][[y]]))
-      matches_per_pseudospectrum <- sapply(nine_matches[[x]][[y]], function(z){
-        SpectrumSimilarity(spec.top = msp_query[[y]][, 1:2], spec.bottom = database[[z]]$pspectrum, print.alignment = F, print.graphic = F)
-      })
-    })
-  })
-  # replace NaN values with 0
-  # allmatches <- lapply(allmatches, function(x){
-  #   x <- lapply(x, function(y){
-  #     y <- sapply(y, function(z){
-  #       z <- ifelse(is.nan(z), 0, z)
-  #     })
-  #   })
-  # })
-  return(allmatches)
-}
-
-match_nines <- function(query_blocks, database_blocks) {
-  #' This function takes the vectors containing the third SPLASH blocks for all pseudospectra.
-  #' Matches the position of the character 9 in the the third blocks in database per sample. 
-  #' Returns list with matches per pseudospectra.
-  db_nines <- str_locate(database_blocks, '9')
-  total_indexes <- vector(mode = 'list', length = length(query_blocks))
-  total_indexes <- lapply(query_blocks, function(x) {
-    query_nines <- str_locate(x, '9')
-    indexes_p_sample <- vector(mode = 'list', length = length(query_nines[, 1]))
-    indexes_p_sample <- lapply(seq_along(query_nines[, 1]), function(y) {
-      indexes <- vector(mode = 'numeric', length = length(db_nines[, 1]))
-      indexes <- sapply(seq_along(db_nines[, 1]), function(z) ifelse(test = query_nines[,1][[y]] == db_nines[, 1][[z]], yes = z, no = 0))
-      indexes <- indexes[indexes != 0]
-    })
-  })
-  return(total_indexes)
-}
-
-
-get_blocks <- function(splashscores, blocknr = 3){
-  #' This function takes all n blocks from the SPLASH codes per pseudospectrum.
-  if (class(splashscores) == "character") {
-    blocks <- str_split(string = splashscores, pattern = '-')
-    blocks <- sapply(blocks, function(x) x[[blocknr]])
-  } else {
-    blocks <- vector(mode = "list", length = length(splashscores))
-    blocks <- lapply(seq_along(splashscores), function(x) {
-      blocks_psample <- vector(mode = 'character', length = length(splashscores[[x]]))
-      blocks_psample <- sapply(splashscores[[x]], function(y) {
-        str_split(string = y, pattern = '-', simplify = F)[[1]][[blocknr]]
-      })
-    })
-  }
-  return(blocks)
-}
-
-
-get_splashscores <- function(msp_object) {
-  #' This function calculates all SPLASH codes per pseudospectrum
-  splashscoresquery <- vector(mode = "character", length = length(msp_object))
-  splashscoresquery <- sapply(seq_along(msp_object), function(x) {
-    lapply(1:length(msp_object[[x]]), function(y) {
-      getSplash(msp_object[[x]][[y]][, 1:2])
-    })
-  })
-  return(splashscoresquery)
-}
-
-
-mSet2xcmsSet <-  function(mSet = smSet_test_data) {
-  xs <- new("xcmsSet")
-  xs@peaks <- mSet[["msFeatureData"]][["chromPeaks"]]
-  rts <- list()
-  if (class(mSet[[2]]) == "OnDiskMSnExp") {
-    format <- "onDiskData"
-  }
-  else {
-    format <- "inMemoryData"
-  }
-  rts$raw <- rtime(mSet[[format]])
-  rts$corrected <- mSet[["msFeatureData"]][["adjustedRT"]]
-  xs@rt <- rts
-  xs@phenoData <- pData(mSet[[format]])
-  xs@filepaths <- fileNames(mSet[[format]])
-  profMethod <- NA
-  profStep <- NA
-  profParam <- list()
-  xs@mslevel <- 1
-  xs@scanrange <- range(scanIndex(mSet[[format]]))
-  if (any(mSet$msFeatureData$chromPeakData$is_filled)) {
-    fld <- which(mSet$msFeatureData$chromPeakData$is_filled)
-    xs@filled <- as.integer(fld)
-  }
-  return(xs)
 }
